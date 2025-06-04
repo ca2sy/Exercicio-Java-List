0e2a7f13bd262e0a87b02972dde384b70748c157
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-06-04 19:54:34 -0300

    Tarefa de operações basicas concluidaa

---
+++ src/ListaDeTarefas.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeTarefas{
+   private List<Tarefa> tarefaLista;
+
+
+   public ListaDeTarefas (){
+    this.tarefaLista = new ArrayList<>();
+
+   }
+
+   public void adicionarTarefa(String descricao){
+     tarefaLista.add(new Tarefa(descricao));
+   }
+
+   public void removerTarefa(String descricao){
+    List<Tarefa> tarefasParaRemover = new ArrayList<>();
+    for(Tarefa t : tarefaLista){
+        if (t.getDescricao().equalsIgnoreCase(descricao)){
+            tarefasParaRemover.add(t);
+        }
+    }
+    tarefaLista.removeAll(tarefasParaRemover);
+   }
+
+
+   public int obterNumeroDeTarefas(){
+    return tarefaLista.size();
+   }
+
+   public void obterDescricoesTarefas(){
+    System.out.println(tarefaLista);
+   }
+}
